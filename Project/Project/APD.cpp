d0ee6b6a12af3434d6ae77abba85de09af40488f
Offuru <andrei.rozmarin@gmail.com>
2024-01-06 21:11:21 +0200

    Added APD class and << operator overload

---
+++ Project/Project/APD.cpp
@@ -0,0 +1,50 @@
+#include "APD.h"
+
+bool APD::isDeterministic() const
+{
+	for (auto& transition : m_transitions)
+	{
+		auto itrStart = m_transitions.lower_bound(transition.first);
+		auto itrEnd = m_transitions.upper_bound(transition.first);
+		--itrEnd;
+		if (itrStart != itrEnd)
+		{
+			return false;
+		}
+	}
+	return true;
+}
+
+std::ostream& operator<<(std::ostream& out, const APD& apd)
+{
+	out << std::format("States:\n");
+	for (const auto& state : apd.m_states)
+		out << std::format("{} ", state);
+	out << "\n\n";
+
+	out << std::format("Alphabet:\n");
+	for (char letter : apd.m_alphabet)
+		out << std::format("{} ", letter);
+	out << "\n\n";
+
+	out << std::format("Stack symbols:\n");
+	for (char letter : apd.m_stackAlphabet)
+		out << std::format("{} ", letter);
+	out << "\n\n";
+
+	out << std::format("Initial state:\n{}\n\n", apd.m_initialState);
+
+	out << std::format("Initial stack state:\n{}\n\n", apd.m_initialStackState);
+
+	out << std::format("Final states:\n");
+	for (const auto& state : apd.m_finalStates)
+		out << std::format("{} ", state);
+	out << "\n\n";
+
+	for (const auto& transition : apd.m_transitions)
+	{
+		const auto& [state, letter, stackState] = transition.first;
+		const auto& [nextState, nextStackState] = transition.second;
+		out << std::format("({},{},{}) -> ({},{})\n", state, letter, stackState, nextState, nextStackState);
+	}
+}
---
+++ Project/Project/APD.h
@@ -0,0 +1,38 @@
+#pragma once
+#include <algorithm>
+#include <iostream>
+#include <queue>
+#include <list>
+#include <vector>
+#include <string>
+#include <map>
+#include <format>
+#include "Grammar.h"
+
+class APD
+{
+public:
+
+	static const char lambda = '$';
+
+	using transition = std::tuple<std::string, char, std::string>;
+	using statePair = std::pair<std::string, std::string>;
+
+	APD(const Grammar& grammar);
+
+	friend std::ostream& operator<<(std::ostream& out, const APD& apd);
+	bool validWord(const std::string& word) const;
+	bool isDeterministic() const;
+
+private:
+
+	std::vector<std::string> m_states;
+	std::vector<char> m_alphabet;
+	std::vector<char> m_stackAlphabet;
+	std::multimap<transition, statePair> m_transitions;
+	std::string m_initialState;
+	std::string m_initialStackState;
+	std::vector<std::string> m_finalStates;
+
+};
+
